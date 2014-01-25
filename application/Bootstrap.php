442e23da740aaa02bd2e8c4b006c4f5d7361d35c
buschagne <itwerkzoekend@gmail.com>
2014-01-25 13:52:35 +0100

    remove autoload in bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,14 +9,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $front->registerPlugin(new App_Plugins_Welcome());
     }
     
-    public function _initAutoloaderNamespaces()
-    {
-        //require_once APPLICATION_PATH . '/../library/vendor/Doctrine/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
-        require_once APPLICATION_PATH . '/../library/Doctrine/Common/ClassLoader.php';
 
-        $autoloader = \Zend_Loader_Autoloader::getInstance();
-        $fmmAutoloader = new \Doctrine\Common\ClassLoader('Bisna');
-        $autoloader->pushAutoloader(array($fmmAutoloader, 'loadClass'), 'Bisna');
-    }
 }
 
